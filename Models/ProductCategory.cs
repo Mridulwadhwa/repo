c68f42653cc63aa9051447b868cc616792fa58da
MridulWadhwa <mridul340@gmail.com>
2020-07-21 21:32:46 +0530

    new

---
+++ Models/ProductCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace Test.Models
+{
+    public class ProductCategory
+    {
+        public ProductCategory()
+        {
+
+        }
+        public ProductCategory(Product p, Category c)
+        {
+            //TO DO: Complete Member Initialization  
+            this.Product = p;
+            this.Category = c;
+        }
+        public Product Product { get; set; }
+        public Category Category { get; set; }
+    }
+}
